a9bfe408d24f19df97cc23f940d82f40f5b8fa40
Piyush Katariya <piyush@stripe.com>
2019-04-18 19:08:28 -0700

    Replaced images and products; added modal title

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ function purchaseProduct(event) {
   alert('hello world');
 }
 
-function openProductModal() {
+function openProductModal(event) {
+  console.log(event.srcElement.parentNode.parentNode.children[0].children[1].innerText)
+  let title=event.srcElement.parentNode.parentNode.children[0].children[1].innerText
+  // console.log(event.srcElement.parentNode.parentNode.innerText)
   $('#purchaseProductModal').modal('show')
+  let productTitle=document.querySelector('#productItemTitle')
+  productTitle.innerHTML=title
+  // let productDescription=document.querySelector('#productItemDescription')
+  // productDescription.innerHTML=description
 }
